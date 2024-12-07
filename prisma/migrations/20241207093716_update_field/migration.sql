21c0393d4b7bc097c2ad3d0af490b004fa59df20
Duck <84608369+Kidkender@users.noreply.github.com>
2024-12-07 22:10:34 +0700

    Delete prisma/migrations directory

--- prisma/migrations/20241207093716_update_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `quizId` on the `course` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `course` DROP COLUMN `quizId`;
